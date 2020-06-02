e6218bc83f613621a55d61a9a6229998e8dd7a73
webhacking <a@hax0r.info>
2020-06-02 14:48:41 +0900

    chore: add me module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { IdentificationModule } from './identification/identification.module';
 import { IdentificationService } from './identification/identification.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { MeModule } from './me/me.module';
 
 @Module({
-  imports: [IdentificationModule, AuthenticationModule],
+  imports: [IdentificationModule, AuthenticationModule, MeModule],
   controllers: [AppController],
   providers: [IdentificationService],
 })
